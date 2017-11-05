4d04c3fa59480eff57cfff5932b93da517d1384b
Tom <thomashollis1@gmail.com>
2017-11-05 13:22:16 +0000

    v1.3

--- InfoGather.sh
+++ InfoGather.sh
@@ -6,13 +6,13 @@ cat /proc/cpuinfo #Processor information
 
 cat /proc/device-tree/model #Model information (only available on recent raspi distro)
 
+sudo apt-get install htop #Install special procmon package
+
+htop #CPU/GPU/RAM usage
+
 free -h -s 1 #RAM usage (updates every second)
 
 cat /etc/debian_version #Debian version information
 
 uname -a #Kernal version information
-
-sudo apt-get install htop #Install special procmon package
-
-htop #CPU/GPU/RAM usage
 
